0daa6ccbb166fcf93bd2df24f06f45bf521f0369
Orkhaan007 <orkhanm07@gmail.com>
2025-04-19 23:17:17 +0400

    V3.5

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -9,9 +9,7 @@ export function Footer() {
       <div className="max-w-[280px]">
         <h3 className="font-semibold text-lg">About</h3>
         <p className="text-gray-600 text-[16px] font-normal mt-3">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam.
+        StackWords is your premier platform for creating and sharing meaningful content. Join our community of writers, thinkers, and creators to explore ideas and connect with readers worldwide.
         </p>
         <div className="text-[16px] font-normal mt-6">
           <p className="mb-1 font-normal text-gray-600"><span className="font-semibold text-[#181A2A]">Email: </span>info@template.net</p>
